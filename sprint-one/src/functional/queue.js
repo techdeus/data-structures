402338da8a5f096423aea35a0c83655a58bdc87f
techdeus <sullivan.marlon@gmail.com>
2018-09-19 15:12:26 -0700

    functional stack and queue complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,41 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var lowestCount = 0;
+  var arr = [];
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    arr.push(lowestCount);
+    storage[lowestCount] = value;
+    count++;
+    lowestCount++;
   };
 
   someInstance.dequeue = function() {
+    var node = storage[arr[0]];
+    delete storage[arr[0]];
+    arr.shift();
+    count--;
+    return node;
   };
 
   someInstance.size = function() {
+    if ( arr.length <= 0 ) {
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
 };
+
+// var queue = Queue();
+// queue.enqueue('a');
+// queue.enqueue('b');
+// console.log(queue.dequeue());
+// queue.enqueue('c');
+
+// console.log(queue.dequeue());
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,35 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {}; // private variable inaccessible to the public
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+  count++;
+  storage[count] = value;
+  return value;
   };
 
   someInstance.pop = function() {
+  
+  var poppedValue = storage[count];
+  delete storage[count];
+  count--;
+  return poppedValue;
   };
 
   someInstance.size = function() {
+    if (count < 0) return 0;
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
+// var newStack = Stack();
+// newStack.push('Marlon');
+// console.log(newStack);
+// // console.log(newStack.someInstance.storage);
+// console.log(newStack.size());
+// console.log(newStack);
