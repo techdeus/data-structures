49dc82e8f069cc57af8bd8a290348c2972ed7726
techdeus <sullivan.marlon@gmail.com>
2018-09-19 20:29:03 -0700

    es6 class for both stack and queue is complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,38 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.obj = {};
+  	this.storage = {};
+  	this.count = 0;
+  	this.lowestCount = 0;
+  	this.arr = [];
+  }
+
+  size() {
+    if ( this.arr.length <= 0 ) {
+      return 0;
+      } else {
+        return this.count;
+      }
+  }
+
+  enqueue(value) {
+    this.arr.push(this.lowestCount);
+    this.storage[this.lowestCount] = value;
+    this.count++;
+    this.lowestCount++;
+  }
+
+  dequeue() {
+    if ( this.size() > 0 ) {
+  	  var node = this.storage[this.arr[0]];
+  	  delete this.storage[this.arr[0]];
+  	  this.arr.shift();
+  	  this.count--;
+  	  return node;	
+  	} else {
+  		return null;
+  	}	
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,32 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+  	this.obj = {};
+  	this.storage = {};
+  	this.count = 0;
+  }
+
+  size() {
+  	return this.count < 0 ? 0 : this.count;
+  }
+
+  push(value) {
+  	this.storage[this.count] = value;
+  	this.count++;
+  	return value;
+  }
+
+  pop() {
+    if (this.size() > 0) {
+      var poppedValue = this.storage[this.count - 1];
+  	  delete this.storage[this.count - 1];
+  	  this.count--;
+  	  return poppedValue;	
+  	} else {
+  		return null;
+  	}
   }
 
 }
